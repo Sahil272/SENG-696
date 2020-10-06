df1f57c964536e6ad1ded1af54431e2bd6b22efd
Sahil272 <sahilpatel.sp272.nr@gmail.com>
2020-10-05 18:12:36 -0600

    commit2

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 public class Temp{
 	public static void main(string[] args){
-		system.out.println("Hello World! Git")
+		system.out.println("Hello World! Git");
+		system.out.println("Print2");
 	}
 }
